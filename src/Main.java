1e22ff8c7236ec118fa46c964d79118486694140
Gabriel Marques <gabbe24081999@gmail.com>
2022-11-29 17:21:01 -0300

    Curso e Mentoria

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+import dominio.Curso;
+import dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso de introdução a Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Curso de introdução a JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição da mentoria de Java");
+        mentoria.setData(LocalDate.of(2022, 12, 15));
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
